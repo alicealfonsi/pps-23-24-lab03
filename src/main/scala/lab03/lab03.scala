cd8862dbfaa4d33d0b86064c164cd291e9029c9f
Alice Alfonsi <alice.alfonsi2@studio.unibo.it>
2024-03-24 18:47:14 +0100

    exercises lab03 completed

--- src/main/scala/lab03/lab03.scala
+++ src/main/scala/lab03/lab03.scala
@@ -1,85 +1,178 @@
 package lab03
 
-import scala.collection.View.FlatMap
-
-object lab03 {
-
-  /**
-   * Task 1
-  */
-  object Sequences:
-    enum Sequence[A]:
-        case Cons(head: A, tail: Sequence[A])
-        case Nil()
+object Sequences03:
+  
+  enum Sequence[A]:
+    case Cons(head: A, tail: Sequence[A])
+    case Nil()
+  
+  object Sequence:
     
-    object Sequence:
+    /**
+      * Task 1
+    */
+    def take[A](l: Sequence[A], n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 => Cons(h, take(t, n - 1))
+      case _ => Nil()
+      
+    def zip[A, B](l: Sequence[A], r: Sequence[B]): Sequence[(A, B)] = (l, r) match
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
+      case _ => Nil()
+      
+    def concat[A](l: Sequence[A], r: Sequence[A]): Sequence[A] = l match
+      case Cons(h, t) => Cons(h, concat(t, r))
+      case _ => r
 
-        def take[A](l: Sequence[A], n: Int): Sequence[A] = l match
-            case Cons(h, t) if n > 0 => Cons(h, take(t, n - 1))
-            case _ => Nil()
-        
-        def zip[A, B](l: Sequence[A], r: Sequence[B]): Sequence[(A, B)] = (l, r) match
-            case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
-            case _ => Nil()
-        
-        def concat[A](l: Sequence[A], r: Sequence[A]): Sequence[A] = l match
-            case Cons(h, t) => Cons(h, concat(t, r))
-            case _ => r
+    def flatMap[A, B](l: Sequence[A])(f: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(f(h), flatMap(t)(f))
+      case _ => Nil()
 
-        def flatMap[A, B](l: Sequence[A])(f: A => Sequence[B]): Sequence[B] = l match
-            case Cons(h, t) => concat(f(h), flatMap(t)(f))
-            case _ => Nil()
-
-        def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = flatMap(l)(a => Cons(mapper(a), Nil()))
-        
-        def filter[A](l: Sequence[A])(pred: A => Boolean): Sequence[A] = flatMap(l)(a => a match
-            case i if pred(a) => Cons(a, Nil())
-            case _ => Nil()    
-        )
-            
-  /**
-   * Task 2
-  */
-  import u03.Optionals.*
-  import Optional.*
-  import lab03.Sequences.*
-  import Sequence.* 
-
-  def min(l: Sequence[Int]): Optional[Int] = l match
-    case Cons(h, t) => min(t) match
-        case Just(value) if value < h => Just(value)
-        case _ => Just(h)      
-    case _ => Empty()
-
-  /**
-   * Task 3
-  */
-  import u02.Modules.Person
-  import u02.Modules.Person.*
-  import lab03.Sequences.*
-  import Sequence.*
-
-  def matchPersonCourse(p: Sequence[Person]): Sequence[String] = p match
-    case Cons(Person.Teacher(_, course), t) => Cons(course, matchPersonCourse(t))
-    case Cons(_: Person.Student, t) => matchPersonCourse(t)
-    case _ => Nil()
-
-  def matchPersonCourseWithFlatMap(p: Sequence[Person]): Sequence[String] =
-    flatMap(p)(p => p match 
-        case Person.Teacher(_, course) => Cons(course, Nil())
-        case _ => Nil()
+    def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = flatMap(l)(a => Cons(mapper(a), Nil()))
+      
+    def filter[A](l: Sequence[A])(pred: A => Boolean): Sequence[A] = flatMap(l)(a => a match
+      case i if pred(a) => Cons(a, Nil())
+      case _ => Nil()    
     )
 
-  /**
-   * Task 4
-  */
-  def foldLeft[A, B](l: Sequence[B])(initialValue: A)(f: (A, B) => A): A = l match
-    case Cons(h, t) => foldLeft(t)(f(initialValue, h))(f)
-    case _ => initialValue
+    /**
+      * Task 2
+    */
+    import u03.Optionals.*
+    import Optional.*
+      
+    def min(l: Sequence[Int]): Optional[Int] = l match
+      case Cons(h, t) => min(t) match
+        case Just(value) if value < h => Just(value)
+        case _ => Just(h)      
+      case _ => Empty()
+
+    /**
+      * Task 3
+    */
+    import u02.Modules.Person
+    import u02.Modules.Person.*
+
+    def matchPersonCourse(p: Sequence[Person]): Sequence[String] = p match
+      case Cons(Person.Teacher(_, course), t) => Cons(course, matchPersonCourse(t))
+      case Cons(_: Person.Student, t) => matchPersonCourse(t)
+      case _ => Nil()
+
+    def matchPersonCourseWithFlatMap(p: Sequence[Person]): Sequence[String] =
+      flatMap(p)(p => p match 
+        case Person.Teacher(_, course) => Cons(course, Nil())
+        case _ => Nil()
+      )  
   
-  /**
-   * Task 5
-  */
+    /**
+      * Task 4
+    */
+    def foldLeft[A, B](l: Sequence[A])(initialValue: B)(f: (B, A) => B): B = l match
+      case Cons(h, t) => foldLeft(t)(f(initialValue, h))(f)
+      case _ => initialValue      
 
+end Sequences03
 
-}
+/**
+  * Task 5
+*/
+object ExtensionMethods:
+
+  import Sequences03.*
+  import Sequences03.Sequence.*
+  
+  extension [A](s: Sequence[A])
+
+    def take(n: Int): Sequence[A] = s match
+      case Cons(h, t) if n > 0 => Cons(h, t.take(n - 1))
+      case _ => Nil()
+
+    def zip[B](r: Sequence[B]): Sequence[(A, B)] = (s, r) match
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), t1.zip(t2))
+      case _ => Nil()
+
+    def concat(r: Sequence[A]): Sequence[A] = s match
+      case Cons(h, t) => Cons(h, t.concat(r))
+      case _ => r
+
+    def flatMap[B](f: A => Sequence[B]): Sequence[B] = s match
+      case Cons(h, t) => f(h).concat(t.flatMap(f))
+      case _ => Nil()
+
+    def map[B](mapper: A => B): Sequence[B] = s.flatMap(a => Cons(mapper(a), Nil()))
+      
+    def filter(pred: A => Boolean): Sequence[A] = s.flatMap(a => a match
+      case i if pred(a) => Cons(a, Nil())
+      case _ => Nil()  
+    )
+
+    def foldLeft[B](initialValue: B)(f: (B, A) => B): B = s match
+      case Cons(h, t) => t.foldLeft(f(initialValue, h))(f)
+      case _ => initialValue
+
+end ExtensionMethods
+
+object Streams03:
+
+  import Sequences03.*
+  import Sequences03.Sequence.*
+
+  enum Stream[A]:
+    private case Empty()
+    private case Cons(head: () => A, tail: () => Stream[A])
+
+  object Stream:
+
+    def empty[A](): Stream[A] = Empty()
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+
+    def toList[A](stream: Stream[A]): Sequence[A] = stream match
+      case Cons(h, t) => Sequence.Cons(h(), toList(t()))
+      case _ => Sequence.Nil()
+
+    def map[A, B](stream: Stream[A])(f: A => B): Stream[B] = stream match
+      case Cons(head, tail) => cons(f(head()), map(tail())(f))
+      case _ => Empty()
+
+    def filter[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if (pred(head())) => cons(head(), filter(tail())(pred))
+      case Cons(head, tail) => filter(tail())(pred)
+      case _ => Empty()
+
+    def take[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match
+      case (Cons(head, tail), n) if n > 0 => cons(head(), take(tail())(n - 1))
+      case _ => Empty()
+
+    def iterate[A](init: => A)(next: A => A): Stream[A] =
+      cons(init, iterate(next(init))(next))
+
+    /**
+     * Task 6
+    */
+    def takeWhile[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail())(pred))
+      case _ => Empty()
+
+    /**
+     * Task 7
+    */
+    def fill[A](n: Int)(element: A): Stream[A] = n match
+      case n if n > 0 => cons(element, fill(n - 1)(element))
+      case _ => Empty()
+    
+    /**
+     * Task 8
+    */
+    def pell(n: Int): Stream[Int] =
+      def computePellNumber(i: Int): Int = i match
+        case 0 => 0
+        case 1 => 1
+        case i if i > 0 => 2 * (computePellNumber(i - 1)) + computePellNumber(i - 2)
+        lazy val str1 = Stream.iterate(0)(_ +1)
+        lazy val str2 = Stream.take(str1)(n)
+        Stream.map(str2)(computePellNumber)   
+    
+end Streams03
--- src/test/scala/lab03/lab03Test.scala
+++ src/test/scala/lab03/lab03Test.scala
@@ -8,7 +8,7 @@ class lab03Test:
   /**
    * Task 1
   */
-  import lab03.Sequences.*
+  import Sequences03.*
   import Sequence.*
 
   val lst1 = Cons(10, Cons(20, Cons(30, Nil())))
@@ -20,36 +20,47 @@ class lab03Test:
     assertEquals(Cons(10, Cons(20, Nil())), take(lst1, 2))
     assertEquals(Nil(), take(lst1, 0))
     assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), take(lst1, 5))
+    import lab03.ExtensionMethods.take
+    assertEquals(Sequence.take(lst1, 5), lst1.take(5)) // Task 5
   }
 
   @Test def testZip(): Unit = {
     assertEquals(Cons((10, "a"), Cons((20, "b"), Cons((30, "c"), Nil()))), zip(lst1, lst2))
+    import lab03.ExtensionMethods.zip
+    assertEquals(Sequence.zip(lst1, lst2), lst1.zip(lst2)) // Task 5
   }
 
   @Test def testConcat(): Unit = {
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), concat(lst3, lst4))
+    import lab03.ExtensionMethods.concat
+    assertEquals(Sequence.concat(lst3, lst4), lst3.concat(lst4)) // Task 5
   }
 
   @Test def testFlatMap(): Unit = {
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(lst1)(v => Cons(v + 1, Nil())))
     assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))),
     flatMap(lst1)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+    import lab03.ExtensionMethods.flatMap
+    assertEquals(Sequence.flatMap(lst1)(v => Cons(v + 1, Nil())), lst1.flatMap(v => Cons(v + 1, Nil()))) // Task 5
   }
 
   @Test def testMap(): Unit = {
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(lst1)(_ + 1))
     assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(lst1)(_ + ""))
+    import lab03.ExtensionMethods.map
+    assertEquals(Sequence.map(lst1)(_ + 1), lst1.map(_ + 1)) // Task 5
   }
 
   @Test def testFilter(): Unit = {
     assertEquals(Cons(20, Cons(30, Nil())), filter(lst1)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(lst1)(_ != 20))
+    import lab03.ExtensionMethods.filter
+    assertEquals(Sequence.filter(lst1)(_ >= 20), lst1.filter(_ >= 20)) // Task 5
   }
   
   /**
    * Task 2
   */
-  import lab03.*
   import u03.Optionals.*
   import Optional.*
 
@@ -85,9 +96,35 @@ class lab03Test:
     assertEquals(-16, foldLeft(listToFold)(0)(_ - _))
     assertEquals(20, foldLeft(listToFold)(4)(_ + _))
     assertEquals(1, foldLeft(emptyListToFold)(1)(_ + _))
+    import lab03.ExtensionMethods.foldLeft
+    assertEquals(Sequence.foldLeft(listToFold)(4)(_ + _), listToFold.foldLeft(4)(_ + _)) // Task 5
+  }
+   
+  /**
+   * Task 6
+  */
+  import Streams03.*
+  import Stream.*
+  
+  @Test def testTakeWhile(): Unit = {
+    val str1 = Stream.iterate(0)(_ + 1)
+    val str2 = Stream.takeWhile(str1)(_ < 5)
+    assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))), Stream.toList(str2))
+  }
+  
+  /**
+   * Task 7
+  */
+  @Test def testFill(): Unit = {
+    assertEquals(Cons("a", Cons("a", Cons("a", Nil()))), Stream.toList(Stream.fill(3)("a")))
+    assertEquals(Nil(), Stream.toList(Stream.fill(0)("a")))
+    assertEquals(Nil(), Stream.toList(Stream.fill(-3)("a")))
   }
 
   /**
-   * Task 5
+   * Task 8
   */
-   
+  @Test def testPell(): Unit = {
+    assertEquals(Cons(0, Cons(1, Cons(2, Cons(5, Cons(12, Nil()))))), Stream.toList(Stream.pell(5)))
+    assertEquals(Cons(0, Cons(1, Cons(2, Cons(5, Cons(12, Cons(29, Cons(70, Nil()))))))), Stream.toList(Stream.pell(7)))
+  }
